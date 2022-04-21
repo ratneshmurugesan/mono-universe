7b5fcedaf960139479c65b132d2bcb145ed7dc9c
ratneshmurugesan <Ratnesh.one@gmail.com>
2022-04-21 12:53:55 +0530

    build(design-system): create bounded-context-level grouping folder - interface(UI)

---
+++ libs/design-system/interface/src/lib/design-system-interface.spec.ts
@@ -0,0 +1,7 @@
+import { designSystemInterface } from './design-system-interface';
+
+describe('designSystemInterface', () => {
+  it('should work', () => {
+    expect(designSystemInterface()).toEqual('design-system-interface');
+  });
+});
