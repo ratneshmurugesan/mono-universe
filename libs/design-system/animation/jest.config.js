c06bd27bf09e768e569b79e549d81d5da28d1261
ratneshmurugesan <Ratnesh.one@gmail.com>
2022-04-21 12:52:24 +0530

    build(design-system): create bounded-context-level grouping folder - animation

--- libs/design-system/animation/jest.config.js
+++ libs/design-system/animation/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'design-system-motion',
+  displayName: 'design-system-animation',
   preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/design-system/motion',
+  coverageDirectory: '../../../coverage/libs/design-system/animation',
 };
