c7d747c2388f818d968d235701f4cc93600ad453
ratneshmurugesan <Ratnesh.one@gmail.com>
2022-04-28 12:30:07 +0530

    build(design-system): update folders/files structure

--- libs/design-system/interface/src/lib/atoms/index.ts
+++
@@ -1,8 +0,0 @@
-import { color, font } from '@mono-universe/design-system/shared/design-tokens';
-
-const interfaceTokens = {
-  headerTextSize: font['size-h2'],
-  boxBgColor: color['purple-darker'],
-};
-
-export { interfaceTokens };
