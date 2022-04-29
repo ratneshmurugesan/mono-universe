ad95ba6cade4fac2ec32e2f41fa0f7597e82022d
ratneshmurugesan <Ratnesh.one@gmail.com>
2022-04-29 14:46:47 +0530

    refactor(design-system-core): remove boilerplate code

--- apps/design-system-core-e2e/src/integration/design-system-core/design-system-core.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('design-system-core: DesignSystemCore component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=designsystemcore--primary'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to DesignSystemCore!');
-    });
-});
