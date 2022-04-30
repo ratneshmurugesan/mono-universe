b2a94daa258595bbbf2763d7c0e9827a0725812c
ratneshmurugesan <Ratnesh.one@gmail.com>
2022-04-30 11:40:27 +0530

    fix(unit-testing): TypeScript & Jest transpliation issue

--- libs/design-system/core/jest.config.ts
+++ libs/design-system/core/jest.config.ts
@@ -2,7 +2,7 @@ module.exports = {
   displayName: 'design-system-core',
   preset: '../../../jest.preset.ts',
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/libs/design-system/core',
