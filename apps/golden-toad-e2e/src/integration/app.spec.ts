e1ca78a262f569ebae91b25c2f251809d857b0d4
ratneshmurugesan <Ratnesh.one@gmail.com>
2022-05-09 08:02:48 +0530

    refactor(golden-toad): remove e2e

--- apps/golden-toad-e2e/src/integration/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { getGreeting } from '../support/app.po';
-
-describe('golden-toad', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome golden-toad');
-  });
-});
