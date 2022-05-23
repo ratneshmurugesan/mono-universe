57468b2efcb6ad9f9d27e5245f494297ddbb4bb8
ratneshmurugesan <Ratnesh.one@gmail.com>
2022-05-23 14:40:49 +0530

    refactor(jest,test): use stories for test

--- libs/design-system/core/src/lib/atoms/button/button.spec.tsx
+++ libs/design-system/core/src/lib/atoms/button/button.spec.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import { act, render, screen } from '@testing-library/react'
 
 import { Base } from './button.stories'
 
@@ -12,5 +12,19 @@ describe('Button', () => {
   it('should render with text content', () => {
     render(<Base {...Base.args}>Base button for test</Base>)
     expect(screen.getByRole('button')).toHaveTextContent(/base/i)
+  })
+
+  it('should render with a snapshot', () => {
+    const { container } = render(<Base {...Base.args} />)
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should react when clicked', () => {
+    const handleOnClick = jest.fn()
+    render(<Base onPress={handleOnClick} />)
+    const buttonElement = screen.getByRole('button')
+    expect(buttonElement).toBeInTheDocument()
+    act(() => buttonElement.click())
+    expect(handleOnClick).toHaveBeenCalledTimes(1)
   })
 })
--- libs/design-system/core/src/lib/atoms/text/text.stories.tsx
+++ libs/design-system/core/src/lib/atoms/text/text.stories.tsx
@@ -1,12 +1,12 @@
 import { Story, Meta } from '@storybook/react'
-import { Text, TextProps } from './text'
+import { Text } from './text'
 
 export default {
   component: Text,
   title: 'Atoms/Text',
 } as Meta
 
-const Template: Story<TextProps> = (args) => <Text {...args} />
+const Template: Story = (args) => <Text {...args} />
 
 export const Primary = Template.bind({})
 Primary.args = {}
