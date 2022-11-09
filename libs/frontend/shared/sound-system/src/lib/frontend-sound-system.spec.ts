75b633887c9ebb7412fa24ff521e43e8f8380c04
ratneshmurugesan <Ratnesh.one@gmail.com>
2022-11-09 14:15:39 +0530

    feat (sound-system): add main hook and mp3 files

--- libs/frontend/shared/sound-system/src/lib/frontend-sound-system.spec.ts
+++ libs/frontend/shared/sound-system/src/lib/frontend-sound-system.spec.ts
@@ -1,7 +1,14 @@
-import { frontendSoundSystem } from './frontend-sound-system'
+import { renderHook, act } from '@testing-library/react'
+import { useSoundSystem } from './frontend-sound-system'
 
-describe('frontendSoundSystem', () => {
-  it('should work', () => {
-    expect(frontendSoundSystem()).toEqual('frontend-sound-system')
+describe('soundSystem', () => {
+  it('should change button state from idle to pressed and with some styles', () => {
+    const { result } = renderHook(() => useSoundSystem())
+    // console.log({ result: typeof result.current.playOnButtonClick })
+    act(() => {
+      result.current.playOnButtonClick()
+    })
+
+    expect(typeof result.current.playOnButtonClick).toEqual('function')
   })
 })
--- libs/frontend/shared/sound-system/src/lib/frontend-sound-system.ts
+++ libs/frontend/shared/sound-system/src/lib/frontend-sound-system.ts
@@ -1,3 +1,17 @@
-export function frontendSoundSystem(): string {
-  return 'frontend-sound-system'
+import useSound from 'use-sound'
+
+export const useSoundSystem = () => {
+  const [playOnButtonClick] = useSound('assets/sounds/ui-hover.mp3', {
+    playbackRate: 2,
+    volume: 1,
+  })
+  const [playOnButtonRelease] = useSound('assets/sounds/ui-hover.mp3', {
+    playbackRate: 3,
+    volume: 0.75,
+  })
+
+  return {
+    playOnButtonClick,
+    playOnButtonRelease,
+  }
 }
