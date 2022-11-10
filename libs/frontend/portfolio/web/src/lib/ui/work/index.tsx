9f6b944666149bbe619d3cfa56340093e8b12447
ratneshmurugesan <Ratnesh.one@gmail.com>
2022-11-10 11:01:44 +0530

    feat: add current + previous portfolio github links

--- libs/frontend/portfolio/web/src/lib/ui/work/index.tsx
+++ libs/frontend/portfolio/web/src/lib/ui/work/index.tsx
@@ -1,16 +1,30 @@
 import { MonoLabel, MonoText } from '@mono-universe/frontend/shared/design-system'
 
 export function Work() {
+  const currentPortfolioLink = 'https://mono-universe-ratnesh-murugesan.vercel.app/'
+  const currentPortfolioGithubRepoLink = 'https://github.com/ratneshmurugesan/mono-universe'
+
+  const previousPortfolioLink = 'https://ratneshs-tech-attic.netlify.app/'
+  const previousPortfolioGithubRepoLink = 'https://github.com/ratneshmurugesan/tech-attic'
+
   const handleRedirect = (url: string) =>
     window.open(url, '_blank', 'noopener,noreferrer,resizable')
   return (
     <>
+      <MonoLabel variant="label">Current portfolio</MonoLabel>
+      <MonoText variant="link" onClick={() => handleRedirect(currentPortfolioLink)}>
+        {currentPortfolioLink}
+      </MonoText>
+      <MonoText variant="link" onClick={() => handleRedirect(currentPortfolioGithubRepoLink)}>
+        {currentPortfolioGithubRepoLink}
+      </MonoText>
+      <br />
       <MonoLabel variant="label">Previous portfolio</MonoLabel>
-      <MonoText
-        variant="link"
-        onClick={() => handleRedirect('https://ratneshs-tech-attic.netlify.app/')}
-      >
-        https://ratneshs-tech-attic.netlify.app/
+      <MonoText variant="link" onClick={() => handleRedirect(previousPortfolioLink)}>
+        {previousPortfolioLink}
+      </MonoText>
+      <MonoText variant="link" onClick={() => handleRedirect(previousPortfolioGithubRepoLink)}>
+        {previousPortfolioGithubRepoLink}
       </MonoText>
     </>
   )
