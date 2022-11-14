362ce1a9d8c27c4698ad3831672d5adbc3aa9fe2
ratneshmurugesan <Ratnesh.one@gmail.com>
2022-11-14 09:48:31 +0530

    feat (ui -> shared -> acme): add router

---
+++ libs/frontend/shared/ui/src/lib/router/index.tsx
@@ -0,0 +1,35 @@
+import { TRouteElementMap, TRouteObj } from '@mono-universe/frontend/shared/common-types'
+import { Suspense } from 'react'
+import { Route } from 'react-router-dom'
+
+export const constructRoute = (routeElementMap: TRouteElementMap, routeObj: TRouteObj) => {
+  const { id, path, index, subPages } = routeObj
+  const _index = index ?? false
+  const Element = routeElementMap[routeObj['id']]
+  if (!subPages.length)
+    return (
+      <Route
+        key={id}
+        index={_index}
+        path={path}
+        element={
+          <Suspense fallback={'Loading..'}>
+            <Element />
+          </Suspense>
+        }
+      />
+    )
+  return (
+    <Route
+      key={id}
+      path={path}
+      element={
+        <Suspense fallback={'Loading..'}>
+          <Element />
+        </Suspense>
+      }
+    >
+      {subPages.map(routeObj => constructRoute(routeElementMap, routeObj))}
+    </Route>
+  )
+}
