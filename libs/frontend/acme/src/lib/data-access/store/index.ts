d93f600d455c9620057079218a6a2ebb03c78d9e
ratneshmurugesan <Ratnesh.one@gmail.com>
2022-11-21 19:16:17 +0530

    fix (acme - test): store setup

--- libs/frontend/acme/src/lib/data-access/store/index.ts
+++ libs/frontend/acme/src/lib/data-access/store/index.ts
@@ -1,14 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import tableSlice from './slice'
 
-export const store = configureStore({
-  reducer: {
-    tableSlice,
-  },
+const rootReducer = combineReducers({
+  tableSlice,
 })
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+const _store = configureStore({
+  reducer: rootReducer,
+})
+
+export function setupStore() {
+  return store
+}
+
+export const store = _store
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof setupStore>
+export type AppDispatch = AppStore['dispatch']
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
--- libs/frontend/acme/src/lib/ui/home/index.test.tsx
+++ libs/frontend/acme/src/lib/ui/home/index.test.tsx
@@ -4,43 +4,38 @@ import '@testing-library/jest-dom'
 import { Provider } from 'react-redux'
 import Home from '.'
 import { CustomCheckbox } from '../components/atoms'
+import { tableSlice } from '../../data-access/store/slice'
+import { ReactElement } from 'react'
+
+export function renderWithProviders(ui: ReactElement) {
+  const store = configureStore({ reducer: { tableSlice: tableSlice.reducer } })
+  function Wrapper({ children }: { children: JSX.Element }) {
+    return <Provider store={store}>{children}</Provider>
+  }
+  return { store, ...render(ui, { wrapper: Wrapper }) }
+}
 
 describe('Acme.Inc', () => {
-  const store = configureStore({
-    reducer: {},
-  })
   test('should render acme component name', () => {
-    render(<Home />)
+    renderWithProviders(<Home />)
     const targetElement = screen.getByText(/Data Table/i)
     expect(targetElement).toBeInTheDocument()
   })
 
   test('should render row checkbox correctly', () => {
-    render(
-      <Provider store={store}>
-        <CustomCheckbox rowObj={{}} />
-      </Provider>,
-    )
+    renderWithProviders(<CustomCheckbox rowObj={{}} />)
     const checkBoxElement = screen.getByRole('checkbox')
     expect(checkBoxElement).toBeInTheDocument()
   })
 
   test('should render no data', () => {
-    render(
-      <Provider store={store}>
-        <Home />
-      </Provider>,
-    )
+    renderWithProviders(<Home />)
     const textElement = screen.getByText('No Data!')
     expect(textElement).toBeInTheDocument()
   })
 
   test('should render header title correctly', async () => {
-    render(
-      <Provider store={store}>
-        <Home />
-      </Provider>,
-    )
+    renderWithProviders(<Home />)
     const textElement = await screen.findByText('title', { selector: 'span' })
     expect(textElement).toBeInTheDocument()
   })
