9f2856eb24897795554840eea440a5d39bbca024
ratneshmurugesan <Ratnesh.one@gmail.com>
2023-12-23 19:24:09 +0530

    feat(app): update recent projects

--- libs/frontend/portfolio/web/src/lib/ui/work/index.tsx
+++ libs/frontend/portfolio/web/src/lib/ui/work/index.tsx
@@ -2,42 +2,35 @@ import { MonoLabel, MonoText } from '@mono-universe/frontend/shared/design-syste
 import { StyledMonoBox } from './styles'
 
 export function Work() {
-  const currentPortfolioLink = 'https://mono-universe-ratnesh-murugesan.vercel.app/'
-  const currentPortfolioGithubRepoLink = 'https://github.com/ratneshmurugesan/mono-universe'
-
   const previousPortfolioLink = 'https://ratneshs-tech-attic.netlify.app/'
-  const previousPortfolioGithubRepoLink = 'https://github.com/ratneshmurugesan/tech-attic'
 
-  const discoverWebSiteLink = 'https://discover-web-ratnesh-x-app.vercel.app/'
-
-  const assetIntelWebSiteLink = 'https://assetintel-ratnesh-x-app.vercel.app/'
+  const shiningCryptoApp = 'https://shining-crypto.vercel.app/'
+  const microEditorApp = 'https://micro-editor.vercel.app/'
+  const flexibleSkeletonApp = 'https://flexible-skeleton-loader.vercel.app/'
+  const classicCrud = 'https://stunning-pavlova-9834d0.netlify.app/'
 
   const handleRedirect = (url: string) =>
     window.open(url, '_blank', 'noopener,noreferrer,resizable')
+
   return (
     <StyledMonoBox>
       <MonoLabel variant="label">Recent projects</MonoLabel>
-      <MonoText variant="link" onClick={() => handleRedirect(discoverWebSiteLink)}>
-        {discoverWebSiteLink}
+      <MonoText variant="link" onClick={() => handleRedirect(shiningCryptoApp)}>
+        {shiningCryptoApp}
       </MonoText>
-      <MonoText variant="link" onClick={() => handleRedirect(assetIntelWebSiteLink)}>
-        {assetIntelWebSiteLink}
+      <MonoText variant="link" onClick={() => handleRedirect(flexibleSkeletonApp)}>
+        {flexibleSkeletonApp}
       </MonoText>
-      <br />
-      <MonoLabel variant="label">Current portfolio</MonoLabel>
-      <MonoText variant="link" onClick={() => handleRedirect(currentPortfolioLink)}>
-        {currentPortfolioLink}
+      <MonoText variant="link" onClick={() => handleRedirect(microEditorApp)}>
+        {microEditorApp}
       </MonoText>
-      <MonoText variant="link" onClick={() => handleRedirect(currentPortfolioGithubRepoLink)}>
-        {currentPortfolioGithubRepoLink}
+      <MonoText variant="link" onClick={() => handleRedirect(classicCrud)}>
+        {classicCrud}
       </MonoText>
       <br />
       <MonoLabel variant="label">Previous portfolio</MonoLabel>
       <MonoText variant="link" onClick={() => handleRedirect(previousPortfolioLink)}>
         {previousPortfolioLink}
-      </MonoText>
-      <MonoText variant="link" onClick={() => handleRedirect(previousPortfolioGithubRepoLink)}>
-        {previousPortfolioGithubRepoLink}
       </MonoText>
     </StyledMonoBox>
   )
